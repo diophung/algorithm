bd0b5c3a242c30f32f58e7ccb3cf4bddfb4b5ccf
Dio Phung <dio.phung@docusign.com>
2020-01-23 23:23:03 -0800

    Test AWS CodeGuru by PR

---
+++ algorithm/array_matrix/ArraySquare.py
@@ -0,0 +1,6 @@
+def find_square(arr1, arr2):
+    """
+    given 2 arrays a1, a2 containing integers, find items in a2 which is 
+    the square of items in a1
+    """
+    return [y for x in arr1, y in arr2 if x*x == y]
