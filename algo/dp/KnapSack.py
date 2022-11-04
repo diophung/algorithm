649000db15908d7db7914d7e282848a2b3780e3e
Dio Phung <phungkimcuong@gmail.com>
2022-11-04 16:28:52 -0700

    Clean up code in Knapsack

--- algo/dp/KnapSack.py
+++ algo/dp/KnapSack.py
@@ -34,33 +34,46 @@ def knapsack_dp(pairs, MAX_W):
     return dp[n][MAX_W]
 
 
-def knapsack_recur(weights, values, W, n):
-    if n == 0 or W == 0:
+def knapsack_recur(weights, values, MAX_W, n):
+    """
+    Given list of weights and values and a MAX_W (weight)
+    Find the optimal value to put in the knapsack
+    Args:
+        weights (list[int]):
+        values (list[int]):
+        MAX_W (int): max weight allowed
+        n (int): size of arrays
+
+    Returns:
+        best value possible
+
+    """
+    if n == 0 or MAX_W == 0:
         return 0
 
     # if this item should not be in the knapsack
-    if weights[n - 1] > W:
-        return knapsack_recur(weights, values, W, n - 1)
+    if weights[n - 1] > MAX_W:
+        return knapsack_recur(weights, values, MAX_W, n - 1)
 
     # else if this item might be in the optimized soln
     # then we choose the better out of
     # 1) include n-th item vs. 2) not include n-th item
     else:
-        included = values[n - 1] + knapsack_recur(weights, values, W - weights[n - 1], n - 1)
-        not_included = knapsack_recur(weights, values, W, n - 1)
+        included = values[n - 1] + knapsack_recur(weights, values, MAX_W - weights[n - 1], n - 1)
+        not_included = knapsack_recur(weights, values, MAX_W, n - 1)
         return max(included, not_included)
 
 
 class KnapSackTest(unittest.TestCase):
+    # pairs of weight-value
+    ITEMS = [(3, 50), (1, 10), (1, 20), (1, 30)]
 
     def test_knap_sack_recursive(self):
-        weight_value_pairs = [(3, 50), (1, 10), (1, 20), (1, 30)]  # W= 3, expect values = 10 + 20 + 30
-        weights = [p[0] for p in weight_value_pairs]
-        values = [p[1] for p in weight_value_pairs]
-        recur = knapsack_recur(weights, values, W=4, n=len(weights))
+        weights = [p[0] for p in self.ITEMS]
+        values = [p[1] for p in self.ITEMS]
+        recur = knapsack_recur(weights, values, MAX_W=4, n=len(weights))
         self.assertEquals(80, recur)
 
     def test_knap_sack_dp(self):
-        weight_value_pairs = [(3, 50), (1, 10), (1, 20), (1, 30)]  # W= 3, expect values = 10 + 20 + 30
-        dp = knapsack_dp(weight_value_pairs, MAX_W=4)
+        dp = knapsack_dp(self.ITEMS, MAX_W=4)
         self.assertEqual(80, dp)
